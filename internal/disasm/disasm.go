4590b457c41400bb1d8a25d4b8efc1d454ebd8f5
hakbyte <hakbyte@users.noreply.github.com>
2022-02-24 12:13:18 +0200

    Define Disasm type

--- internal/disasm/disasm.go
+++ internal/disasm/disasm.go
@@ -8,6 +8,11 @@ const (
 
 // Disasm represents an disassembled program in memory
 type Disasm struct {
+	text       []byte            // .text data
+	ib         uint64            // ImageBase
+	bc         uint64            // BaseOfCode
+	mode       uint64            // processor mode
+	disasmList map[uint64]string // disassembled .text data
 }
 
 // New opens a program file and reads its content for later disassembly
