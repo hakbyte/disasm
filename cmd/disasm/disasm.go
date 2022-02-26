332d7af82168278ef4e635ef840da87eb46633cb
hakbyte <hakbyte@users.noreply.github.com>
2022-02-26 06:19:52 +0200

    Implement `Usage()`

--- cmd/disasm/disasm.go
+++ cmd/disasm/disasm.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/hakbyte/disasm/internal/disasm"
@@ -8,4 +9,10 @@ import (
 
 func main() {
 	_ := disasm.New(os.Args[1])
+}
+
+// Usage prints the program usage to stdout and exit
+func Usage() {
+	fmt.Print("Usage:\n\tdisasm.exe <file.exe>\n")
+	os.Exit(1)
 }
